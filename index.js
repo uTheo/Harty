71ab2d383653ebbaf3744696147a7ccccfe0decf
Sazzo <stolensoul32@gmail.com>
2020-04-28 22:42:53 -0300

    owo

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ client.on("message", async message => {
   if(message.content.indexOf(prefix) !== 0) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+  let verified = message.guild.roles.cache.get("498294566483656707");
+  let noVerified = message.guild.roles.cache.get("567787227258552333")
   if(command === "verify") {
   try {
   	const user = client.users.cache.get(message.author.id)
      const response = await axios.get(`https://verify.eryn.io/api/user/${message.author.id}`)
       if(response.data.status === "ok") {
-      let verified = message.guild.roles.cache.get("498294566483656707");
-      let noVerified = message.guild.roles.cache.get("567787227258552333")
       message.member.roles.add(verified).catch(console.error);
       message.member.roles.remove(noVerified).catch(console.error);
       message.channel.send(`Seja Bem-Vindo, **${response.data.robloxUsername}**, espero que tenha bom proveito dos chats do servidor (=`)
@@ -66,5 +66,24 @@ client.on("message", async message => {
   	console.log(`[LOG] ${user.tag} tentou executar o comando whois, porém sem sucesso.`)
   	}
  }
+ if(command === "reverse") {
+   message.member.roles.remove(verified).catch(console.error);
+   message.member.roles.add(noVerified).catch(console.error);
+   message.channel.send("Seu cargo de verificado foi retirado.")
+ }
 })
+client.on('guildMemberAdd', async member => {
+  try {
+    const response = await axios.get(`https://verify.eryn.io/api/user/${member.id}`)
+    if(response.data.status === "ok") {
+     let verified = member.guild.roles.cache.get("498294566483656707");
+     let noVerified = member.guild.roles.cache.get("567787227258552333")
+     console.log(`[AutoVerify] ${member.displayName} foi verificado ao entrar com a conta ${response.data.robloxUsername} (${response.data.robloxId})`)
+     member.roles.add(verified).catch(console.error);
+     member.roles.remove(noVerified).catch(console.error);
+    }
+  } catch(e) {
+  console.log(`[AutoVerify] ${member.displayName} não existe no banco de dados da RoVer, ou aconteceu um erro inesperado.`)
+  }
+});
 client.login(process.env.TOKEN)
