2bd475b1654eb59065c76e4862a1cf5a4f99d3d0
Sazzo <stolensoul32@gmail.com>
2020-05-29 20:11:33 -0300

    feat

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('message', async message => {
     } catch(e){
       if(e.response.status === 404) {
         let success = false
-        message.channel.send('(Isso está em BETA, se você tiver problemas me envie uma mensagem no privado <@326123612153053184>):warning: | Por favor, se verifique em https://verify.eryn.io (Clique em Sign With Discord) | Esperando confirmação da ``API``, assim que for obtido você será verificado.')
+        message.channel.send('(Isso está em BETA, se você tiver problemas me envie uma mensagem no privado <@326123612153053184>) | :warning: | Por favor, se verifique em https://verify.eryn.io (Clique em Sign With Discord) | Esperando confirmação da ``API``, assim que for obtido você será verificado.')
         const b = setTimeout(() => {
           if(success = false) {
             clearInterval(x)
